aefa135fc92d9e8c55f82d0a2039f8a6873dae3c
Pedro Giampietro <pedrogiampietro@hotmail.com>
2020-07-06 17:19:04 -0300

    Adicionado algumas implementações sign-in!

--- links-frontend/src/screens/SignIn/SignInReducer.js
+++ links-frontend/src/screens/SignIn/SignInReducer.js
@@ -1,14 +1,29 @@
 import { SIGN_IN } from './SignInActions';
+import { setAccount, setToken, setRefreshToken } from '../../helpers/account';
+
 
 const initialState = {
   account: null,
 }
 
 export default function(state = initialState, action){
-  const { type, payload } = action;
-  switch(type){
+  const { type, payload } = action
+
+  switch(type) {
     case SIGN_IN:
-      return{ ...initialState, account: payload }
+
+      const response = payload ? payload.data : null;
+      const account = response ? response.data : null;
+      const metadata = response ? response.metadata : null;
+
+      const token = metadata ? metadata.token : null;
+      const refreshToken = metadata ? metadata.refreshToken : null;
+
+      if(account) setAccount(account);
+      if(token) setToken(token);
+      if(refreshToken) setRefreshToken(refreshToken);
+
+      return{ ...initialState, account }
     default:
       return state;
   }
