c93be4630cdef94ad745095108274b6ecbe352ee
Pedro Giampietro <pedrogiampietro@hotmail.com>
2020-07-06 17:09:43 -0300

    adicionado signup & signin

--- links-frontend/src/screens/SignUp/SignUpReducer.js
+++ links-frontend/src/screens/SignUp/SignUpReducer.js
@@ -1,13 +1,33 @@
 import { SIGN_UP } from './SignUpActions';
+import { setAccount, setToken, setRefreshToken } from '../../helpers/account';
 
 const initialState = {
   account: null,
 }
 
-export default function(state = initialState, action){
-  const { type, payload } = action;
+export default function(state = initialState, action) {
+  const { type, payload } = action
+
+  //console.log('***SignUpReducer.type', type)
+
   switch(type){
     case SIGN_UP:
+      const response = payload ? payload.data : null;
+      const account = response ? response.data : null;
+      const metadata = response ? response.metadata : null;
+
+      const token = metadata ? metadata.token : null;
+      const refreshToken = metadata ? metadata.refreshToken : null;
+
+      //console.log('***SignUpReducer.account', account)
+      //console.log('***SignUpReducer.token', token)
+      //console.log('***SignUpReducer.refreshToken', refreshToken)
+
+      
+      if(account) setAccount(account);
+      if(token) setToken(token);
+      if(refreshToken) setRefreshToken(refreshToken);
+
       return{ ...initialState, account: payload }
     default:
       return state;
