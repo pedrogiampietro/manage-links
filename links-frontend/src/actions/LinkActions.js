1a943d0f662feb35287274504ddefedef2142eaa
Pedro Giampietro <pedrogiampietro@hotmail.com>
2020-07-08 15:24:44 -0300

    finalizando botão de delete, crud 5/5

--- links-frontend/src/actions/LinkActions.js
+++ links-frontend/src/actions/LinkActions.js
@@ -1,10 +1,11 @@
-import { apiPost, apiGet, apiPut } from '../helpers/api'
+import { apiPost, apiGet, apiPut, apiDelete } from '../helpers/api'
 
 export const LINK_CREATE = 'LINK_CREATE'
 export const LINK_LIST = 'LINK_LIST'
 export const LINK_GET = 'LINK_GET'
 export const LINK_UPDATE = 'LINK_UPDATE'
 export const LINK_TO_REMOVE = 'LINK_TO_REMOVE'
+export const LINK_REMOVE = 'LINK_REMOVE'
 
 export const linkCreate = (data) => {
   const isSocial = !!data.isSocial
@@ -30,4 +31,9 @@ export const linkList = (data) => {
 
 export const setLinkToRemove = (link) => {
   return { type: LINK_TO_REMOVE, payload: link }
+}
+
+export const linkRemove = (link) => {
+  const payload = apiDelete(`/link/${link.id}`)
+  return { type: LINK_REMOVE, payload }
 }
--- links-frontend/src/screens/Manage/Links/index.js
+++ links-frontend/src/screens/Manage/Links/index.js
@@ -2,13 +2,16 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom'
 import Layout from '../../Layouts/Manage'
-import { linkList, setLinkToRemove } from '../../../actions/LinkActions'
+import { linkList, setLinkToRemove, linkRemove } from '../../../actions/LinkActions'
 
-const Links = ({ links, linkToRemove, linkList, setLinkToRemove }) => {
+const Links = ({ links, linkToRemove, linkRemove, linkList, setLinkToRemove }) => {
 
     useEffect(() => {
         linkList()
     }, [linkList])
+
+    const cancelDelete = (e) => setLinkToRemove(null)
+    const confirmDelete = (e) => (linkToRemove ? linkRemove(linkToRemove) : null)
 
     return (
         <Layout>
@@ -50,7 +53,19 @@ const Links = ({ links, linkToRemove, linkList, setLinkToRemove }) => {
                 )
             }) 
             : null }
-          
+
+
+              {linkToRemove ? (  
+             <div className="alert alert-danger rounded float-center shadow-bold">
+                 <h4 className="alert-heading">Delete Confirmation!</h4>
+                 <p>Are you sure you want to delete, this action cannot be undone.</p>
+                 <hr />
+                 <div className="d-flex justify-content-between">
+                     <button className="btn btn-outline-light" onClick={cancelDelete}>Cancel</button>
+                     <button className="btn btn-danger" onClick={confirmDelete}>Delete</button>
+                 </div>
+             </div>
+                ) : null }
         </Layout>
     )
 }
@@ -63,4 +78,4 @@ const MapStateToProps = (state) => {
 }
 
 
-export default connect(MapStateToProps, { linkList, setLinkToRemove })(Links)
+export default connect(MapStateToProps, { linkList, setLinkToRemove, linkRemove })(Links)
